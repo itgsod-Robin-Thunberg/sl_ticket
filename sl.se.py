90eae9f4fe75ab24890e8e83d84174180738f436
robin.thunberg <itgsod-Robin-Thunberg>
2017-10-18 14:00:08 +0200

    Sl tickets change

--- sl.se.py
+++ sl.se.py
@@ -1,26 +1,26 @@
 def fares(age, student=False, senior=False):
     if age>=20 and age <65:
         if student==True:
-            print "price is 20kr"
+            return "price is 20kr"
         elif senior==True:
-            print "price is 20kr"
+            return "price is 20kr"
 
         else:
-            print "price is 30kr"
+            return "price is 30kr"
     elif age <20 and age >65:
         if student==True:
-            print "price is 20kr"
+            return "price is 20kr"
         elif senior==True:
-            print "price is 20kr"
+            return "price is 20kr"
     else:
-        print "price is 20kr"
+        return "price is 20kr"
 
 
 
 
 
 
-fares(70)
-fares(50)
-fares(25,student=True)
-fares(15,student=True)
+print fares(70)
+print fares(50)
+print fares(25,student=True)
+print fares(15,student=True)
